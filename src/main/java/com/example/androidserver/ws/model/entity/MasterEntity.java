07beb6226d523d209ef8943841194445e186e109
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2024-05-10 19:48:08 +0330

    fix Conflict

--- src/main/java/com/example/androidserver/ws/model/entity/MasterEntity.java
+++ src/main/java/com/example/androidserver/ws/model/entity/MasterEntity.java
@@ -1,36 +1,20 @@
 package com.example.androidserver.ws.model.entity;
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-<<<<<<< HEAD
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-=======
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
->>>>>>> 4e9c0481a9e4203076b88d62ca6fdcdd618267ab
-
 import java.util.List;
 
 @Entity
 @Table
-<<<<<<< HEAD
 @Data
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
-=======
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
->>>>>>> 4e9c0481a9e4203076b88d62ca6fdcdd618267ab
 public class MasterEntity {
 
     @Id
